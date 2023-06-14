6177a86e9b1e4c7a7c07693d65f2a5e9384b5f72
SM-Heang <mheang67@gmail.com>
2023-06-14 10:02:42 +0700

    test push

--- app/Http/Controllers/ArticleCategoryController.php
+++ app/Http/Controllers/ArticleCategoryController.php
@@ -20,7 +20,7 @@ class ArticleCategoryController extends Controller
      * Show the form for creating a new resource.
      */
     public function create()
-    {
+    { 
         return view('articles.categories.create');
     }
 
